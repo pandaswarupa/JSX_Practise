a09704e3825db4753e98aa564f9aef8d84c795a9
Swarupa <panda.swarupa5@gmail.com>
2024-10-31 22:03:25 +0530

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+ 
+var React = require('react');
+var ReactDom = require('react-dom');
+
+ReactDom.render(<h1>Hello World!</h1>,document.getElementById('root'));
